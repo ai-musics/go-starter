80a065b6118bfe6273f59c700013c3bdae895559
mars <mars@syneforge.com>
2021-03-11 18:32:29 +0200

    fix

--- app/cmd/starter/main.go
+++ app/cmd/starter/main.go
@@ -3,9 +3,42 @@ package main
 import (
 	"fmt"
 	"github.com/ai-musics/go-starter/internal/greet"
+	"github.com/foolin/goview"
+	"github.com/foolin/goview/supports/echoview-v4"
+	"github.com/labstack/echo/v4"
+	"net/http"
+	"html/template"
+	"time"
 )
 
 func main() {
+	// Echo instance
+	e := echo.New()
+
+	// Create renderer
+	gv := echoview.New(goview.Config{
+		Root:      "views",
+		Extension: ".html",
+		Master:    "layouts/application",
+		Funcs: template.FuncMap{
+			"copy": func() string {
+				return time.Now().Format("2006")
+			},
+		},
+	})
+
+	// Renderer
+	e.Renderer = gv
+
+	e.GET("/", hello)
+
+	e.Logger.Fatal(e.Start(":3001"))
+}
+
+func hello(c echo.Context) error {
 	fmt.Println("Go modules!")
-	fmt.Println(greet.TheWorld())
+
+	return c.Render(http.StatusOK, "index", echo.Map{
+		"Title": greet.TheWorld(),
+	})
 }
--- app/go.mod
+++ app/go.mod
@@ -1,3 +1,8 @@
 module github.com/ai-musics/go-starter
 
 go 1.15
+
+require (
+	github.com/foolin/goview v0.3.0 // indirect
+	github.com/labstack/echo/v4 v4.2.1 // indirect
+)
